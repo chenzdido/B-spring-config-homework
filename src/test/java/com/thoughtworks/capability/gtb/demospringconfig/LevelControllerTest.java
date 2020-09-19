38a2a6e2c5745f94b67c27839532a3086e55d628
chenzdido <chenzdido@gmail.com>
2020-09-20 00:10:59 +0800

    [zhe] feat:complete p2s3.

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,56 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LevelControllerTest1 {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private LevelController levelController;
+    @Test
+    public void shouldGetBasicLevelWhenLevelNumberIsLessOne() throws Exception {
+        String  result=mockMvc.perform(get("/level")).andReturn().getResponse().getContentAsString();
+        assertEquals(result, "basic");
+    }
+}
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("t1")
+class LevelControllerTest2 {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private LevelController levelController;
+    @Test
+    public void shouldGetBasicLevelWhenLevelNumberIsLessOne() throws Exception {
+        String  result=mockMvc.perform(get("/level")).andReturn().getResponse().getContentAsString();
+        assertEquals(result, "advanced");
+    }
+}
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("t2")
+class LevelControllerTests3{
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private LevelController levelController;
+
+    @Test
+    public void shouldGetBasicLevelWhenLevelNumberIsGreaterOne() throws Exception {
+        String  result=mockMvc.perform(get("/level")).andReturn().getResponse().getContentAsString();
+        assertEquals(result, "advanced");
+    }
+}
